c686a9bb2441205a9007a2154fe557e6ff80d967
Eric Ye <eric.ye@facteon.global>
2020-12-22 14:33:06 +1300

    fixed demoapp Dockerfile

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as fp:
 
 setuptools.setup(
     name="aip",
-    version="0.0.10",
+    version="0.0.13",
 
     description="An empty CDK Python app",
     long_description=long_description,
