8d41a4bcfce058524e4d7168b494c7bd07859953
Eric Ye <hongqiang.ye@gmail.com>
2020-12-24 13:05:30 +1300

    bumping tag to 0.1.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md") as fp:
 
 setuptools.setup(
     name="aip",
-    version="0.1.5",
+    version="0.1.6",
 
     description="An empty CDK Python app",
     long_description=long_description,
